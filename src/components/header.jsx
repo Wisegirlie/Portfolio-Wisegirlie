e6d32a8a9e0fe7aebe1cd617ff717f3138271646
Gabriela Waisman <gabywaisman@hotmail.com>
2025-03-30 21:36:29 -0400

    added responsive menu

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,9 +7,14 @@
 import { Link } from 'react-router-dom';
 import '../css/header.css'
 import logo from '../assets/logo_gaby_bolder.png';
+import hamburger from "../assets/hamburger.png";
+import { useState } from 'react';
+
 
 export function Header(props) {
 
+    const [responsiveMenu, setResponsiveMenu] = useState(false);
+    
     const navButtons = [
             {key: "home", name: "Home", location: "/"},
             {key: "about", name: "About me", location: "/about"},
@@ -25,14 +30,36 @@ export function Header(props) {
                     </li>
                  </Link>
                 );                      
+
+    function toggleResponsiveMenu() {
+        setResponsiveMenu(!responsiveMenu);    
+    }
+
   return (
-    <header>
-      <Link to="/">
-        <img src={logo} alt="Logo Gabriela Waisman" className="logo-img"/>     
-      </Link>
-      <nav>
-        <ul>{menuitems}</ul>   
-      </nav>     
-    </header>
+      <header>
+          <Link to="/">
+              <img
+                  src={logo}
+                  alt="Logo Gabriela Waisman"
+                  className="logo-img"
+              />
+          </Link>
+          <nav>
+              {/*  hamburger for responsive menu */}
+              <div className="hamburger">
+                  <button onClick={toggleResponsiveMenu}>
+                      <img
+                          src={hamburger}
+                          alt="Menu Mobile Icon"
+                          className="hamburgerImg"
+                      />
+                  </button>
+              </div>
+              {/* menu items */}
+              <ul className={`nav-ul ${responsiveMenu ? 'show-responsive-menu' : ''}`} id="menu">
+                  {menuitems}
+              </ul>
+          </nav>
+      </header>
   );
 }
