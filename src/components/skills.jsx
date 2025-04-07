428064e0c79f7cf2f3838517292de244b973e36c
Gabriela Waisman <gabywaisman@hotmail.com>
2025-04-07 12:32:37 -0400

    corrected hidden space in skills courses

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -203,9 +203,9 @@ export default function Skills() {
                     </div>
                 </div>
 
+                {/* <br />
                 <br />
-                <br />
-                <br />
+                <br /> */}
 
                 {/* ---- COURSES ---- */}
                 <h1 className="hide">Certifications & Extra Courses</h1>
